a31e0b0a16db15b524a55c9317cea22b5d25e89d
Marlin Schäfer <mb.schaefer@gmx.de>
2021-09-30 16:16:45 +0200

    Fixed bug where sensitivity was not calculated correctly.
    Added some documentation.

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-"""Docstring
+"""A program to calculate the false-alarm rate as well as the sensitive
+distance from a search algorithm. (Part of the MLGWSC-1)
 """
 import argparse
 import numpy as np
@@ -8,6 +9,32 @@ import os
 import logging
 
 def find_injection_times(fgfiles, injfile, padding_start=0, padding_end=0):
+    """Determine injections which are contained in the file.
+    
+    Arguments
+    ---------
+    fgfiles : list of str
+        Paths to the files containing the foreground data (noise +
+        injections).
+    injfile : str
+        Path to the file containing information on the injections in the
+        foreground files.
+    padding_start : {float, 0}
+        The amount of time (in seconds) at the start of each segment
+        where no injections are present.
+    padding_end : {float, 0}
+        The amount of time (in seconds) at the end of each segment
+        where no injections are present.
+    
+    Returns
+    -------
+    duration:
+        A float representing the total duration (in seconds) of all
+        foreground files.
+    bool-indices:
+        A 1D array containing bools that specify which injections are
+        contained in the provided foreground files.
+    """
     duration = 0
     times = []
     for fpath in fgfiles:
@@ -33,7 +60,76 @@ def find_injection_times(fgfiles, injfile, padding_start=0, padding_end=0):
     
     return duration, np.any(ret, axis=0)
 
+def find_closest_index(array, value, assume_sorted=False):
+    """Find the index of the closest element in the array for the given
+    value(s).
+    
+    Arguments
+    ---------
+    array : np.array
+        1D numpy array.
+    value : number or np.array
+        The value(s) of which the closest array element should be found.
+    assume_sorted : {bool, False}
+        Assume that the array is sorted. May improve evaluation speed.
+    
+    Returns
+    -------
+    indices:
+        Array of indices. The length is determined by the length of
+        value. Each index specifies the element in array that is closest
+        to the value at the same position.
+    """
+    if not assume_sorted:
+        array = array.copy()
+        array.sort()
+    ridxs = np.searchsorted(array, value, side='right')
+    lidxs = np.maximum(ridxs - 1, 0)
+    comp = np.fabs(array[lidxs] - value) < \
+           np.fabs(array[np.minimum(ridxs, len(array) - 1)] - value)
+    lisbetter = np.logical_or((ridxs == len(array)), comp)
+    ridxs[lisbetter] -= 1
+    return ridxs
+
 def get_stats(fgevents, bgevents, injparams, duration=None):
+    """Calculate the false-alarm rate and sensitivity of a search
+    algorithm.
+    
+    Arguments
+    ---------
+    fgevents : np.array
+        A numpy array with three rows. The first row has to contain the
+        times returned by the search algorithm where it believes to have
+        found a true signal. The second row contains a ranking statistic
+        like quantity for each time. The third row contains the maxmimum
+        distance to an injection for the given event to be counted as
+        true positive. The values have to be determined on the
+        foreground data, i.e. noise plus additive signals.
+    bgevents : np.array
+        A numpy array with three rows. The first row has to contain the
+        times returned by the search algorithm where it believes to have
+        found a true signal. The second row contains a ranking statistic
+        like quantity for each time. The third row contains the maxmimum
+        distance to an injection for the given event to be counted as
+        true positive. The values have to be determined on the
+        background data, i.e. pure noise.
+    injparams : dict
+        A dictionary containing at least two entries with keys `tc` and
+        `distance`. Both entries have to be numpy arrays of the same
+        length. The entry `tc` contains the times at which injections
+        were made in the foreground. The entry `distance` contains the
+        according luminosity distances of these injections.
+    duration : {None or float, None}
+        The duration of the analyzed background. If None the injections
+        are used to infer the duration.
+    
+    Returns
+    -------
+    dict:
+        Returns a dictionary, where each key-value pair specifies some
+        statistic. The most important are the keys `far` and
+        `sensitive-distance`.
+    """
     ret = {}
     injtimes = injparams['tc']
     dist = injparams['distance']
@@ -44,19 +140,8 @@ def get_stats(fgevents, bgevents, injparams, duration=None):
     fgevents = fgevents.T[sidxs].T
     
     logging.info('Finding injection times closest to event times')
-    maxidxs = np.searchsorted(injtimes, fgevents[0], side='right')
-    minidxs = np.maximum(maxidxs - 1, 0)
-    maxidxs = np.minimum(maxidxs, len(injtimes) - 1)
-    lowdiff = np.abs(injtimes[minidxs] - fgevents[0])
-    highdiff = np.abs(injtimes[maxidxs] - fgevents[0])
-    
-    mmidxs = np.vstack([minidxs, maxidxs])
-    lhdiff = np.vstack([lowdiff, highdiff])
-    tmpidxs = np.argmin(lhdiff, axis=0)
-    boolidxs = np.vstack([tmpidxs == 0, tmpidxs == 1])
-    #The indices below are the indices of the closest injections
-    idxs = mmidxs[boolidxs]
-    diff = lhdiff[boolidxs]
+    idxs = find_closest_index(injtimes, fgevents[0])
+    diff = np.abs(injtimes[idxs] - fgevents[0])
     
     logging.info('Finding true- and false-positives')
     tpidxs = np.arange(len(fgevents[0]))[diff <= fgevents[2]]
@@ -81,8 +166,6 @@ def get_stats(fgevents, bgevents, injparams, duration=None):
     logging.info('Calculating foreground FAR')
     noise_stats = fpevents[1].copy()
     noise_stats.sort()
-    # fgfar = len(noise_stats) - np.searchsorted(noise_stats, tpevents[1],
-    #                                            side='left')
     fgfar = len(noise_stats) - np.arange(len(noise_stats)) - 1
     fgfar = fgfar / duration
     ret['fg-far'] = fgfar
@@ -92,8 +175,6 @@ def get_stats(fgevents, bgevents, injparams, duration=None):
     logging.info('Calculating background FAR')
     noise_stats = bgevents[1].copy()
     noise_stats.sort()
-    # far = len(noise_stats) - np.searchsorted(noise_stats, tpevents[1],
-    #                                          side='left')
     far = len(noise_stats) - np.arange(len(noise_stats)) - 1
     far = far / duration
     ret['far'] = far
@@ -108,10 +189,8 @@ def get_stats(fgevents, bgevents, injparams, duration=None):
     Ninj = len(dist)
     prefactor = vtot / Ninj
     
-    # nfound = len(tp_sort) - np.searchsorted(tp_sort, tpevents[1],
-    #                                         side='left')
     nfound = len(tp_sort) - np.searchsorted(tp_sort, noise_stats,
-                                            side='left')
+                                            side='right')
     sample_variance = nfound / Ninj - (nfound / Ninj) ** 2
     vol = prefactor * nfound
     vol_err = prefactor * (Ninj * sample_variance) ** 0.5
@@ -148,8 +227,6 @@ def main(doc):
     parser.add_argument('--output-file', type=str, required=True,
                         help=("Path at which to store the output HDF5 "
                               "file. (Path must end in `.hdf`)"))
-    # parser.add_argument('--duration', type=float,
-    #                     help="Set the duration of analyzed data.")
     
     parser.add_argument('--verbose', action='store_true',
                         help="Print update messages.")
