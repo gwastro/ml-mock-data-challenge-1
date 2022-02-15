8edce82a1e1c4b09713ec92c1dd8acaceae9b3e5
Marlin Schäfer <mb.schaefer@gmx.de>
2022-02-15 11:40:06 +0100

    PEP8 compliance.

--- contributions/whiten.py
+++ contributions/whiten.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
-
-### Import modules
+"""Strain whitening script to be used for development of submission to the
+MLGWSC-1 mock data challenge. Written by Ondřej Zelenka.
+"""
+# Import modules
 from argparse import ArgumentParser
 import logging
 import h5py
@@ -10,33 +12,37 @@ import numpy as np
 from pycbc.psd import inverse_spectrum_truncation, interpolate
 import pycbc.types
 
+
 def copy_attrs(in_obj, out_obj):
     for key, attr in in_obj.attrs.items():
         out_obj.attrs[key] = attr
     return
+
 
 class SafeCaster:
     def __init__(self, dtype_bits):
         if dtype_bits is None:
             self.dtype = None
         elif isinstance(dtype_bits, int):
-            if dtype_bits%8==0:
+            if dtype_bits % 8 == 0:
                 self.dtype = np.dtype('f%i' % (dtype_bits//8))
             else:
                 raise ValueError("Data type in bits must"
-                                "be divisible by 8.")
+                                 "be divisible by 8.")
         else:
             raise ValueError
+
     def __call__(self, in_arr):
         if self.dtype is None:
             return in_arr
         else:
             return in_arr.astype(self.dtype)
 
+
 def whiten(strain, delta_t=1./2048., segment_duration=0.5,
-                max_filter_duration=0.25, trunc_method='hann',
-                remove_corrupted=True, low_frequency_cutoff=None,
-                psd=None, **kwargs):
+           max_filter_duration=0.25, trunc_method='hann',
+           remove_corrupted=True, low_frequency_cutoff=None, psd=None,
+           **kwargs):
     """Whiten a strain.
 
     Arguments
@@ -75,18 +81,18 @@ def whiten(strain, delta_t=1./2048., segment_duration=0.5,
         The whitened time series.
 
     """
-    if strain.ndim==1:
+    if strain.ndim == 1:
         colored_ts = pycbc.types.TimeSeries(strain, delta_t=delta_t)
         if psd is None:
             psd = colored_ts.psd(segment_duration, **kwargs)
         elif isinstance(psd, np.ndarray):
-            assert psd.ndim==1
+            assert psd.ndim == 1
             logging.warning("WARNING: Assuming PSD delta_f based "
-                "on delta_t, length of PSD and EVEN length "
-                "of original time series. Tread carefully!")
+                            "on delta_t, length of PSD and EVEN length "
+                            "of original time series. Tread carefully!")
             assumed_duration = delta_t*(2*len(psd)-2)
             psd = pycbc.types.FrequencySeries(psd,
-                    delta_f=1./assumed_duration)
+                                              delta_f=1./assumed_duration)
         elif isinstance(psd, pycbc.types.FrequencySeries):
             pass
         else:
@@ -95,90 +101,97 @@ def whiten(strain, delta_t=1./2048., segment_duration=0.5,
         max_filter_len = int(max_filter_duration*colored_ts.sample_rate)
 
         psd = inverse_spectrum_truncation(psd,
-                    max_filter_len=max_filter_len,
-                    low_frequency_cutoff=low_frequency_cutoff,
-                    trunc_method=trunc_method)
+                                          max_filter_len=max_filter_len,
+                                          low_frequency_cutoff=low_frequency_cutoff,  # noqa: E501
+                                          trunc_method=trunc_method)
 
         # inv_psd = np.array([0. if num==0. else 1./num for num in psd])
         inv_psd = 1./psd
-        white_ts = (colored_ts.to_frequencyseries()*inv_psd**0.5
-                ).to_timeseries()
+        white_ts = (colored_ts.to_frequencyseries()
+                    * inv_psd**0.5).to_timeseries()
 
         if remove_corrupted:
-            white_ts = white_ts[max_filter_len//2:(len(colored_ts)-max_filter_len//2)]
+            kmin = max_filter_len//2
+            kmax = (len(colored_ts)-max_filter_len//2)
+            white_ts = white_ts[kmin:kmax]
 
         return white_ts.numpy()
 
-    elif strain.ndim==2:
+    elif strain.ndim == 2:
         psds_1d = None
         if isinstance(psd, np.ndarray):
-            if psd.ndim==1:
+            if psd.ndim == 1:
                 psds_1d = [psd for _ in strain]
         if psds_1d is None:
-            if (psd is None) or isinstance(psd,
-                                pycbc.types.FrequencySeries):
+            if (psd is None) or isinstance(psd, pycbc.types.FrequencySeries):
                 psds_1d = [psd for _ in strain]
             else:
-                assert len(psd)==len(strain)
+                assert len(psd) == len(strain)
                 psds_1d = psd
 
         white_segments = [whiten(sd_strain, delta_t=delta_t,
-                segment_duration=segment_duration,
-                max_filter_duration=max_filter_duration,
-                trunc_method=trunc_method,
-                remove_corrupted=remove_corrupted,
-                low_frequency_cutoff=low_frequency_cutoff, psd=psd_1d,
-                **kwargs) for sd_strain, psd_1d in zip(strain, psds_1d)]
+                                 segment_duration=segment_duration,
+                                 max_filter_duration=max_filter_duration,
+                                 trunc_method=trunc_method,
+                                 remove_corrupted=remove_corrupted,
+                                 low_frequency_cutoff=low_frequency_cutoff,
+                                 psd=psd_1d,
+                                 **kwargs)
+                          for sd_strain, psd_1d in zip(strain, psds_1d)]
         return np.stack(white_segments, axis=0)
 
     else:
         raise ValueError("Strain numpy array dimension must be 1 or 2.")
 
 
-def main():
-    parser = ArgumentParser(description="Strain whitening script "
-        "to be used for development of submission to the MLGWSC-1 "
-        "mock data challenge. Written by Ondřej Zelenka.")
+def main(desc):
+    parser = ArgumentParser(description=desc)
 
     parser.add_argument('--verbose', action='store_true',
-            help="Print update messages.")
+                        help="Print update messages.")
     parser.add_argument('--debug', action='store_true',
-            help="Show debug messages.")
+                        help="Show debug messages.")
     parser.add_argument('--force', action='store_true',
-            help="Overwrite existing output file.")
+                        help="Overwrite existing output file.")
 
-    parser.add_argument('inputfile', type=str, help="The path to the "
-        "input data file. It should be of the format generated "
-        "by the generate_data.py script.")
-    parser.add_argument('outputfile', type=str, help="The path where "
-        "to store the whitened data. The file must not exist.")
+    parser.add_argument('inputfile', type=str,
+                        help="The path to the "
+                             "input data file. It should be of the format "
+                             "generated by the generate_data.py script.")
+    parser.add_argument('outputfile', type=str,
+                        help="The path where to store the whitened data. The "
+                             "file must not exist.")
     parser.add_argument('-s', '--segment-duration', type=float,
-        default=0.5, help="Duration in seconds to use for each sample "
-        "of the spectrum for PSD estimation. Default: 0.5.")
+                        default=0.5,
+                        help="Duration in seconds to use for each sample of "
+                             "the spectrum for PSD estimation. Default: 0.5.")
     parser.add_argument('-d', '--max-filter-duration', type=float,
-        default=0.25, help="Maximum length of the time-domain filter "
-        " in seconds. Default: 0.25.")
+                        default=0.25,
+                        help="Maximum length of the time-domain filter in "
+                             "seconds. Default: 0.25.")
     parser.add_argument('--hard-trunc', action='store_true',
-        help="Use a hard truncation of the time-domain filter. "
-        "Otherwise, a Hann window is used.")
+                        help="Use a hard truncation of the time-domain "
+                             "filter. Otherwise, a Hann window is used.")
     parser.add_argument('--keep-corrupted', action='store_true',
-        help="Keep the region of the time series corrupted "
-        "by the whitening. Otherwise, it is excised.")
+                        help="Keep the region of the time series corrupted "
+                             "by the whitening. Otherwise, it is excised.")
     parser.add_argument('-f', '--low-frequency-cutoff', type=float,
-        default=20., help="Low frequency cutoff to be passed to the "
-        "inverse spectrum truncation. This should be matched "
-        "to a known low frequency cutoff of the data if there is one. "
-        "Default: 20.")
+                        default=20.,
+                        help="Low frequency cutoff to be passed to the "
+                             "inverse spectrum truncation. This should be "
+                             "matched to a known low frequency cutoff of the "
+                             "data if there is one. Default: 20.")
     parser.add_argument('--dtype', type=int, default=None,
-        help="Data type specified by number of bits. If supplied, "
-        "the input data will be cast to the corresponding "
-        "floating point data type before whitening.")
+                        help="Data type specified by number of bits. If "
+                             "supplied, the input data will be cast to the "
+                             "corresponding floating point data type before "
+                             "whitening.")
     parser.add_argument('--compress', action='store_true',
-        help="Compress the output file.")
+                        help="Compress the output file.")
 
     args = parser.parse_args()
 
-    ### Set up logging
+    # Set up logging
     if args.debug:
         log_level = logging.DEBUG
     elif args.verbose:
@@ -186,9 +199,10 @@ def main():
     else:
         log_level = logging.WARN
     logging.basicConfig(format="%(levelname)s | %(asctime)s: "
-            "%(message)s", level=log_level, datefmt='%d-%m-%Y %H:%M:%S')
+                        "%(message)s", level=log_level,
+                        datefmt='%d-%m-%Y %H:%M:%S')
 
-    ### Check existence of output file
+    # Check existence of output file
     if os.path.isfile(args.outputfile) and not args.force:
         raise RuntimeError("Output file exists.")
     else:
@@ -200,11 +214,11 @@ def main():
         trunc_method = 'hann'
 
     logging.debug(("Initializing caster to data type %s"
-        % str(args.dtype)))
+                  % str(args.dtype)))
     caster = SafeCaster(args.dtype)
     if args.compress:
         ds_write_kwargs = {'compression': 'gzip', 'compression_opts': 9,
-                            'shuffle': True}
+                           'shuffle': True}
     else:
         ds_write_kwargs = {}
 
@@ -213,26 +227,27 @@ def main():
         total = sum([len(grp) for grp in infile.values()])
         copy_attrs(infile, outfile)
         with tqdm(desc='Whitening individual datasets',
-            disable=not args.verbose, ascii=True, total=total) as pbar:
+                  disable=not args.verbose, ascii=True, total=total) as pbar:
             for detector_group_name, in_detector_group in infile.items():
                 out_detector_group = outfile.create_group(detector_group_name)
                 copy_attrs(in_detector_group, out_detector_group)
                 for segment_name, in_segment in in_detector_group.items():
                     segment_colored_data = in_segment[()]
                     segment_white_data = whiten(segment_colored_data,
-                        delta_t=1./infile.attrs['sample_rate'],
-                        segment_duration=args.segment_duration,
-                        max_filter_duration=args.max_filter_duration,
-                        trunc_method=trunc_method,
-                        remove_corrupted=(not args.keep_corrupted),
-                        low_frequency_cutoff=args.low_frequency_cutoff)
+                                                delta_t=1./infile.attrs['sample_rate'],  # noqa: E501
+                                                segment_duration=args.segment_duration,  # noqa: E501
+                                                max_filter_duration=args.max_filter_duration,  # noqa: E501
+                                                trunc_method=trunc_method,
+                                                remove_corrupted=(not args.keep_corrupted),  # noqa: E501
+                                                low_frequency_cutoff=args.low_frequency_cutoff)  # noqa: E501
                     out_segment = out_detector_group.create_dataset(
                         segment_name, data=caster(segment_white_data),
                         **ds_write_kwargs)
                     copy_attrs(in_segment, out_segment)
                     if not args.keep_corrupted:
-                        out_segment.attrs['start_time'] += args.max_filter_duration/2
+                        out_segment.attrs['start_time'] += args.max_filter_duration/2  # noqa: E501
                     pbar.update(1)
 
-if __name__=='__main__':
-    main()
+
+if __name__ == '__main__':
+    main(__doc__)
